7789c6fae360dcb15491427eeb86f73138cdb4ed
sbseligman <sbseligman@comcast.net>
2016-02-25 18:18:26 +0000

    Adding index file

---
+++ work.js
@@ -0,0 +1,15 @@
+function helloworld() {
+    console.log("Hello World");
+    };
+function addNo(No1, No2) {
+    var sum = No1 + No2;
+    console.log(sum);
+};
+
+var person = { 
+    name:"Spencer",
+    age: [16,26,36],
+    location: "Brooklyn, NY"
+};
+
+// JavaScript File

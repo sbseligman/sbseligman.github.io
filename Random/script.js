a2efe1c32a07738ba6cee29ac5dd9a43d9069121
sbseligman <sbseligman@comcast.net>
2016-03-10 05:03:03 +0000

    random

--- Random/script.js
+++ Random/script.js
@@ -1,14 +1,11 @@
-$("body").css("background-color", "#ff8875");
+$(".area").css("background-color", "#ff8875");
 
 var shape = ["square", "circle", "triangle"]
 var hue = ["#ff8875", "#ff8875","#ff8875", "#fea367", "#fba46a", "#f7a56e",  "#f4a571", "#f0a675", "#eca779", "#e8a87d", "#e4a981", "#e0a985", "#ddaa88", "#d9ab8c", "#d5ac90", "#d1ac94", "#cdad98", "#c9ae9c", "#c6af9f", "#c2afa3", "#beb0a7", "#bab1ab", "#b6b2af", "#b3b3b3"]
 
 
-for (var i = 0; i < 500; i++) {
-    var randshape = shape[Math.floor(Math.random() * shape.length)];
-    
-    $("body").append("<div class='box' " + randshape + "></div>");
-    $(".box").each(function sizeposcolor() {
+function sizeposcolor() {
+    $(".box").each(function() {
         var randtop = Math.floor(Math.random() * 1000) - 10 + "px";
         var randleft = Math.floor(Math.random() * 1000) -10 + "px";
         var randsize = Math.floor(Math.random() * 100) -10 + "px";
@@ -22,3 +19,19 @@ for (var i = 0; i < 500; i++) {
 };
 
 
+
+$(".area").mousemove(function() {
+   for (var i = 0; i < 15; i++){
+   $("body").append("<div class='box'></div>")
+   };
+   sizeposcolor();
+    var x = event.clientX - 15 + "px";
+    var y = event.clientY - 15 + "px";
+    $("body").append("<div class='circle'></div>");
+    $(".circle").css("left", x);
+    $(".circle").css("top", y);
+    $(".circle").html("<img src='sun.png' class='sun'>");
+    $(".sun").css("position", "relative");
+    $(".sun").css("top", - 80 + "px");
+    $(".sun").css("left", - 80 + "px");
+});

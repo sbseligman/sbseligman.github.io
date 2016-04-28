a38d510043d654ab6a26889b1a8ad6c62abed078
sbseligman <sbseligman@comcast.net>
2016-04-28 13:44:08 +0000

    sunrise

--- Random/script.js
+++ Random/script.js
@@ -19,12 +19,12 @@ $(window).mousemove(function(e) {
     $("#mount4").css("opacity", opac);
     var newbg = "rgba(34,59,103," + e.clientY/$(window).height() + ")";
     $(".sky").css("background-color",newbg)
-    $("#mount1").css("left", - 300 -  x * 0.025 );
-    $("#mount1b").css("left", - 300 -  x * 0.025 );
+    $("#mount1").css("left", - 300 -  x * 0.035 );
+    $("#mount1b").css("left", - 300 -  x * 0.035 );
     $("#mount2").css("left", - 50 -  x * 0.01 );
     $("#mount2b").css("left", - 50 -  x * 0.01 );
-    $("#mount3").css("left", + 300 -  x * 0.05 );
-    $("#mount3b").css("left", + 300 -  x * 0.05 );
+    $("#mount3").css("left", + 300 -  x * 0.08 );
+    $("#mount3b").css("left", + 300 -  x * 0.08 );
     $("#mount4").css("left", + 900 -  x * 0.01 );
     $("#mount4b").css("left", + 900 -  x * 0.01 );
     var suntop = y +"px";

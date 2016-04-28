8614517fc0e4ca136bd54cc697792794169eb124
sbseligman <sbseligman@comcast.net>
2016-04-28 15:54:27 +0000

    sunrise added moon and fixed stars

--- Random/script.js
+++ Random/script.js
@@ -1,9 +1,9 @@
-for (var i = 0; i < 30; i++) {
+for (var i = 0; i < 50; i++) {
     $(".starsky").append("<div class='star'></div>");
 };
 $(".star").each(function(){
-         var randtop = Math.floor(Math.random() * window.innerHeight) + "px";
-         var randleft = Math.floor(Math.random() * window.innerWidth) + "px";
+         var randtop = Math.floor(Math.random() * window.innerHeight) + 0.05 + "px";
+         var randleft = Math.floor(Math.random() * window.innerWidth) + 0.05 + "px";
          $(this).css("top", randtop);
          $(this).css("left", randleft);
     });
@@ -19,20 +19,25 @@ $(window).mousemove(function(e) {
     $("#mount4").css("opacity", opac);
     var newbg = "rgba(34,59,103," + e.clientY/$(window).height() + ")";
     $(".sky").css("background-color",newbg)
-    $("#mount1").css("left", - 300 -  x * 0.035 );
-    $("#mount1b").css("left", - 300 -  x * 0.035 );
-    $("#mount2").css("left", - 50 -  x * 0.01 );
-    $("#mount2b").css("left", - 50 -  x * 0.01 );
-    $("#mount3").css("left", + 300 -  x * 0.08 );
-    $("#mount3b").css("left", + 300 -  x * 0.08 );
-    $("#mount4").css("left", + 900 -  x * 0.01 );
-    $("#mount4b").css("left", + 900 -  x * 0.01 );
-    var suntop = y +"px";
+    $("#mount1").css("left", - 300 -  x * 0.05 );
+    $("#mount1b").css("left", - 300 -  x * 0.05 );
+    $("#mount2").css("left", - 50 -  x * 0.03 );
+    $("#mount2b").css("left", - 50 -  x * 0.03 );
+    $("#mount3").css("right", + 50 +  x * 0.1 );
+    $("#mount3b").css("right", + 50 +  x * 0.1 );
+    $("#mount4").css("right", - 400 +  x * 0.03 );
+    $("#mount4b").css("right", - 400 +  x * 0.03 );
+    var suntop = y +"px"
     $(".sun").css("top", suntop);
     $(".sunb").css("top", suntop);
+    $(".moon").css("bottom", y*0.8 - 100 + "px");
     $(".sun").css("opacity", opac);
     $(".star").css("opacity", opac);
-    
-    
+    $(".moon").css("opacity", opac + 0.25);
+    $(".starsky").css("height", window.innerHeight + "px");
+    $(".starsky").css("width", window.innerWidth + "px");
+    var angle = (e.clientX/window.innerWidth)*135 + "deg";
+    console.log(angle);
+    $(".starsky").css("transform", "rotate(" + angle + ")");
     
 });
